7d1ffdef6c8dbe6f6f716701ea6c4a7169eef7e1
M-eporo <155133725+M-eporo@users.noreply.github.com>
2024-05-05 13:15:42 +0900

    Update mysidebar.php

--- mysidebar.php
+++ mysidebar.php
@@ -4,6 +4,8 @@
       <h2 class="news-title">Archives</h2>
         <ul class="sidebar-list">
           <?php wp_get_archives('type=yearly'); ?>
+          <?php wp_get_archives('post-type=post&type=yearly&show_post_count=1'); ?>
+          <?php wp_get_archives('post-type=post&type=yearly&format=option&affter=年'); ?>
         </ul>
     </div>
     <div class="item">
